42309287a2d07fa9abae1ea2cc6f4bdf0fb58bfc
LiChihTseng <lichih.tseng@uqconnect.edu.au>
2026-08-27 23:15:37 +0800

    fix: let vercel resolve node types for api functions

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,7 @@
 import { inject } from "vue";
 import About from "../components/About.vue";
 // import ConsultationBridge from "../components/ConsultationBridge.vue";
-import ConsultationForm from "../components/ConsultationForm.vue";
+// import ConsultationForm from "../components/ConsultationForm.vue";
 import Contact from "../components/Contact.vue";
 import Footer from "../components/Footer.vue";
 import Hero from "../components/Hero/Hero.vue";
